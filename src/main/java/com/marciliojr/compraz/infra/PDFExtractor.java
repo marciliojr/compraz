a330a91846bdd4417e131294c0f867b43bcb4e1f
Marcilio Jr <marcilio3@gmail.com>
2025-02-02 21:36:51 -0300

    Test git actions

--- src/main/java/com/marciliojr/compraz/infra/PDFExtractor.java
+++ src/main/java/com/marciliojr/compraz/infra/PDFExtractor.java
@@ -7,17 +7,8 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 import java.io.IOException;
 
-/**
- * Classe responsável por extrair texto de arquivos PDF e converter MultipartFile em File.
- */
 public class PDFExtractor {
 
-    /**
-     * Extrai o texto de um arquivo PDF fornecido.
-     *
-     * @param pdfFile Arquivo PDF do qual o texto será extraído.
-     * @return O texto extraído do PDF ou {@code null} se ocorrer um erro.
-     */
     public String extrairTextoPDF(File pdfFile) {
         if (pdfFile == null || !pdfFile.exists()) {
             System.err.println("Erro: Arquivo PDF não encontrado ou inválido.");
@@ -40,13 +31,6 @@ public class PDFExtractor {
         return null;
     }
 
-    /**
-     * Converte um MultipartFile (arquivo enviado via HTTP) em um objeto File.
-     *
-     * @param multipartFile O arquivo enviado via HTTP.
-     * @return Um arquivo temporário representando o MultipartFile.
-     * @throws IOException Se ocorrer um erro ao criar ou transferir o arquivo.
-     */
     public File converterParaArquivo(MultipartFile multipartFile) throws IOException {
         if (multipartFile == null || multipartFile.isEmpty()) {
             throw new IllegalArgumentException("O MultipartFile está vazio ou é nulo.");
